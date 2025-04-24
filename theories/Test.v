9b46052843fb2074559bf4f7de725a8a781dca2f
MathisBD <mathis.bouverot@ens.psl.eu>
2025-04-24 14:27:49 +0200

    finish tweaking build system

--- theories/Test.v
+++ theories/Test.v
@@ -3,6 +3,6 @@ Declare ML Module "autosubst-experiments.plugin".
 Inductive term :=
 | Var : nat -> term
 | App : term -> term -> term 
-| Lam : string -> term -> term.
+| Lam : (*string ->*) term -> term.
 
 Test term.
